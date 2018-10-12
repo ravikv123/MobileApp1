2a79aa1343a89766634940533e158b5bc7ee84f3
Ravi <linuxhome8@gmail.com>
2018-10-12 21:37:15 +0530

    login updates

---
+++ src/pages/login/login.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController } from 'ionic-angular';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio';
+import {HomePage} from '../home/home';
+ 
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+ 
+  constructor(public navCtrl: NavController, private faio: FingerprintAIO) {
+  }
+
+  login() {
+          //this.	navCtrl.setRoot(HomePage);
+
+    this.faio.show({
+      clientId: 'Fingerprint-Demo',
+      clientSecret: 'password', // Only Android
+      localizedFallbackTitle: 'Use Pin', // Only iOS
+      localizedReason: 'Please authenticate' // Only iOS
+    })
+      .then((result: any) => {
+        this.navCtrl.setRoot(HomePage);
+      })
+      .catch((error: any) => {
+        console.log('err: ', error);
+      });
+  }
+}
